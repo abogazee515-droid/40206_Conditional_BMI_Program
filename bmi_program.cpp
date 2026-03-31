7b721497af3260ecae3704ff5b69a9e3cedf7be0
abogazee515-droid <abogazee515@gmail.com>
2026-03-31 18:06:47 +0700

    Add ReadData procedure

--- bmi_program.cpp
+++ bmi_program.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+// Procedure to read data
+void ReadData(float &weight, float &height)
+{
+    cout << "Masukkan berat badan (kg): ";
+    cin >> weight;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> height;
+}
